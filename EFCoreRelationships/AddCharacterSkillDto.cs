f746e565a72563d46e7ffb82de1daf8a3683c9b1
Durgesh Kumar <durgeshkumarguptaprince@outlook.com>
2022-03-29 16:32:34 +0530

    Add project files.

---
+++ EFCoreRelationships/AddCharacterSkillDto.cs
@@ -0,0 +1,9 @@
+﻿namespace EFCoreRelationships
+{
+    public class AddCharacterSkillDto
+    {
+        public int CharacterId { get; set; }
+
+        public int SkillId { get; set; }
+    }
+}
---
+++ EFCoreRelationships/User.cs
@@ -0,0 +1,10 @@
+﻿namespace EFCoreRelationships
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string username { get; set; } = String.Empty;
+
+        public List<Character> Characters { get; set; }
+    }
+}
